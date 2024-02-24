433cf91ef405ac37a63a9aa37281c57db66d102a
kimilguk <boramcom@daum.net>
2024-02-24 14:36:09 +0900

    3회차 2교시 작업소스입니다.

--- src/main/java/com/kimilguk/boot3/KimilgukBoot3Application.java
+++ src/main/java/com/kimilguk/boot3/KimilgukBoot3Application.java
@@ -3,7 +3,9 @@ package com.kimilguk.boot3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA공통DB필드(변수)사용
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class KimilgukBoot3Application {
 	//현재 앱의 진입 메소드가 자동 생성 된다.
