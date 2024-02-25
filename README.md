1c974a11935cc5e788c54554da066164719065f4
kimilguk <boramcom@daum.net>
2024-02-25 12:32:29 +0900

    4회차 3교시 작업소스입니다.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### 네트워크프로그래밍실습 강의용 깃 소스 입니다.
-#### 4회차 2교시 작업소스입니다.
+#### 4회차 3교시 작업소스입니다.
 - [교수의 모든 주차_교시별 강의용소스 링크](https://github.com/miniplugin/kimilguk-boot3/branches/all)
--- src/main/java/com/kimilguk/boot3/web/IndexController.java
+++ src/main/java/com/kimilguk/boot3/web/IndexController.java
@@ -9,36 +9,59 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.kimilguk.boot3.domain.posts.Posts;
 import com.kimilguk.boot3.service.posts.PostsService;
+import com.kimilguk.boot3.web.dto.PostsDto;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor//final 매개변수가 있는 생성자메소드가 자동 생성된다
-@Controller//일반컨트롤러는 반환값으로 출력할 페이지를 지정한다
+@RequiredArgsConstructor // final 매개변수가 있는 생성자메소드가 자동 생성된다
+@Controller // 일반컨트롤러는 반환값으로 출력할 페이지를 지정한다
 public class IndexController {
-	//로그 출력 객체생성
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    private final PostsService postsService;//생성자로 주입
-    
-    @GetMapping("/posts/save")//Url주소와 posts-save.mustache를 매핑 시킨다.
-    public String postsSave() {
-        return "posts/posts-save";
+	// 로그 출력 객체생성
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	private final PostsService postsService;// 생성자로 주입
+
+	@GetMapping("/posts/update/{id}")//패스경로에 id값이 들어갔다. @PathVariable 사용해서 id값을 자바코드에서 사용
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsDto dto = postsService.postsOne(id);//1개의 레코드만 가져온다.
+        model.addAttribute("post",dto);//모델객체에 담아서 mustache로 보낸다.
+        if(dto.getFileId() != null) {
+            //단일 첨부파일 처리는 이후 수업에서 작업예정 
+        }
+        return "posts/posts-update";
     }
-    
-    @GetMapping("/")//전체게시물 Read
-    public String postList(@PageableDefault(size=5,sort="id",direction=Sort.Direction.DESC) Pageable pageable, Model model) {
-    Page<Posts> postsList = postsService.postsList(pageable);
-    model.addAttribute("postsList", postsList);//게시글목록 5개
-    model.addAttribute("currPage", postsList.getPageable().getPageNumber());//현재페이지번호
-    model.addAttribute("pageIndex", postsList.getTotalPages());//전체페이지개수
-    model.addAttribute("prevCheck", postsList.hasPrevious());//이전페이지 있는지 체크
-    model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());//이전페이지번호 사용
-    model.addAttribute("nextCheck", postsList.hasNext());//다음페이지 있는지 체크
-    model.addAttribute("next", pageable.next().getPageNumber());//다음페이지번호 사용
-    //return "posts/post-list";//출력할 페이지명 posts폴더/post-list.mustache파일(html디자인템플릿)
-    return "index"; //index.mustache에서 확장자가 생략된 표현이다.
-    } // mustache템플릿 html 뷰 파일은 다음시간에 만든다.
-  //index.mustache 파일이 templates 폴더 최상위에 있기 때문에 경로 없이 파일명을 사용 가능하다.
+	
+	@GetMapping("/posts/read/{id}") // 패스경로에 id값이 들어갔다. @PathVariable 사용해서 자바코드에서 사용
+	public String postsRead(@PathVariable Long id, Model model) {
+		PostsDto dto = postsService.postsOne(id);// 1개의 레코드만 가져온다.
+		model.addAttribute("post", dto);// 모델객체에 담아서 mustache로 보낸다.
+		if (dto.getFileId() != null) {
+			// 단일 첨부파일 처리는 이후 수업에서 작업예정
+		}
+		return "posts/posts-read";
+	}
+
+	@GetMapping("/posts/save") // Url주소와 posts-save.mustache를 매핑 시킨다.
+	public String postsSave() {
+		return "posts/posts-save";
+	}
+
+	@GetMapping("/") // 전체게시물 Read
+	public String postList(@PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+			Model model) {
+		Page<Posts> postsList = postsService.postsList(pageable);
+		model.addAttribute("postsList", postsList);// 게시글목록 5개
+		model.addAttribute("currPage", postsList.getPageable().getPageNumber());// 현재페이지번호
+		model.addAttribute("pageIndex", postsList.getTotalPages());// 전체페이지개수
+		model.addAttribute("prevCheck", postsList.hasPrevious());// 이전페이지 있는지 체크
+		model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());// 이전페이지번호 사용
+		model.addAttribute("nextCheck", postsList.hasNext());// 다음페이지 있는지 체크
+		model.addAttribute("next", pageable.next().getPageNumber());// 다음페이지번호 사용
+		// return "posts/post-list";//출력할 페이지명 posts폴더/post-list.mustache파일(html디자인템플릿)
+		return "index"; // index.mustache에서 확장자가 생략된 표현이다.
+	} // mustache템플릿 html 뷰 파일은 다음시간에 만든다.
+	// index.mustache 파일이 templates 폴더 최상위에 있기 때문에 경로 없이 파일명을 사용 가능하다.
 }
